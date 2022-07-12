dfaa42cecca7b15534dab39207f781fbfd184c58
Igwe Acha Akwu <successoracha001@gmail.com>
2022-07-12 08:10:59 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function isPowerTwo(number) {
+  if (number <= 1) return false;
+
+  let divisibleByTwo = number;
+  while (divisibleByTwo !== 1) {
+    if (divisibleByTwo % 2 == 1) {
+      return false;
+    }
+    divisibleByTwo /= 2;
+  }
+  return true;
+}
+
+console.log(isPowerTwo(10));
